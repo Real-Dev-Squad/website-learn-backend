9c0da2543e1bee88cfd1f2f2b83572de6fb48c13
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-04-06 21:55:18 +0530

    fix formatting

--- src/main.go
+++ src/main.go
@@ -12,12 +12,14 @@ func main() {
 	value, keyExists := os.LookupEnv("env")
 	port := strconv.Itoa(8000)
 	version := strconv.Itoa(1)
+
 	if keyExists {
 		log.Printf("Running in %v mode\n", value)
 	} else {
 		log.Printf("Running in local dev mode\n")
 		value = "local"
 	}
+
 	router := routes.SetupRouter(value, version)
 	log.Printf("Server is running on http://localhost:%v\n", port)
 	router.Run("localhost:" + port)
