2c3078fbcea1b4de9449542814287a801b2ffa5c
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-04-08 15:04:56 +0530

    set gin to release mode in prod

--- src/routes/router.go
+++ src/routes/router.go
@@ -3,6 +3,9 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func SetupRouter(env string) *gin.Engine {
+	if env == "prod" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	router := gin.Default()
 	UseRoutes(router)
 	return router
