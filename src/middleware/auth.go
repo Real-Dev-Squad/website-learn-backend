29680cdd12d11717d9b8ceb496647c18417c0da3
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-04-11 13:58:46 +0530

    add auth middleware and an authenticated route

---
+++ src/middleware/auth.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"github.com/Real-Dev-Squad/gopher-cloud-service/src/config"
+	"github.com/Real-Dev-Squad/gopher-cloud-service/src/utils"
+	jwt "github.com/dgrijalva/jwt-go"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+
+	authenticatedUser := true
+
+	cookie, err := context.Cookie(config.Global.CookieName)
+
+	if err == nil {
+		key, err := jwt.ParseRSAPublicKeyFromPEM([]byte(config.Global.PublicKey))
+
+		if err == nil {
+			parsedJWT, _, err := new(jwt.Parser).ParseUnverified(cookie, jwt.MapClaims{})
+
+			if err == nil {
+				if claims, ok := parsedJWT.Claims.(jwt.MapClaims); ok {
+					userId := claims["userId"]
+					context.Set("userId", userId)
+				}
+
+				token, err := jwt.Parse(cookie, func(token *jwt.Token) (interface{}, error) { return key, nil })
+
+				if token.Valid || err == nil {
+					context.Next()
+				} else {
+					authenticatedUser = false
+				}
+			} else {
+				authenticatedUser = false
+			}
+		} else {
+			authenticatedUser = false
+		}
+	} else {
+		authenticatedUser = false
+	}
+
+	if !authenticatedUser {
+		utils.UnauthorizedResponse(context)
+	}
+}
